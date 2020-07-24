42603b38cb8a710489ae63b355376db8f9f3847d
bratinkundu <bkundu53@gmail.com>
2020-07-24 09:59:07 +0530

    Created project and added the UserMicroservice

---
+++ UserMicroservice/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UserMicroservice.Models;
+
+namespace UserMicroservice.Controllers
+{
+    [Route("api/User")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("GetAllUsers")]
+        public IActionResult GetAllUsers()
+        {
+            List<Users> users = UsersDummyData();
+            return Ok(users);
+        }
+
+        public List<Users> UsersDummyData()
+        {
+            IList<Users> Users = new List<Users>()
+            {
+                new Users()
+                {
+                     Id = 1,
+                     Name ="User 1",
+                     Email = "user1@g.co"
+                },
+                new Users()
+                {
+                    Id = 2,
+                    Name = "User 2",
+                    Email = "user2@g.co"
+                },
+                new Users()
+                {
+                    Id = 3,
+                    Name = "User 3",
+                    Email = "user3@g.co"
+                }
+            };
+
+            return Users.ToList();
+        }
+    }
+}
